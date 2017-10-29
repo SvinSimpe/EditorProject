cc74b444dd2a84a7aae4ca90d9f1940897e08d33
Simon Johanson <johanson.simon@hotmail.com>
2017-10-29 12:11:44 +0100

    Added note to move some libs

--- Editor/EditorProject/stdafx.h
+++ Editor/EditorProject/stdafx.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#pragma comment( lib, "d3dcompiler.lib" )
+/*
+	*Note: The d3dcompiler lib is only used by the Renderer
+	so it should only be visible to it and no other object.
+*/
+
+#pragma comment( lib, "d3dcompiler.lib" ) 
 #pragma comment( lib, "d3d11.lib" )
 #pragma comment( lib, "dxgi.lib" )
 
